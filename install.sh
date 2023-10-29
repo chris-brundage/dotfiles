1eb9e4da44f48ea65d698085a98dc82df6acdcdd
Chris Brundage <christopher.m.brundage@gmail.com>
2023-10-28 21:30:33 -0500

    Working install script

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ SRC="${1:-${HOME}/src/dotfiles}"
 
 # Get all directories inside source dir, except the dir itself and .git, and stow things
 find "${SRC}" ! -path "${SRC}" -maxdepth 1 -type d -not -name .git -and -not -name . -exec basename {} \; | while read -r stow_dir; do
-    echo "stow --target=${HOME} ${stow_dir}"
+    printf "Stowing %s\n" "${stow_dir}"
 
-    stow --delete "${stow_dir}"
-    stow "${stow_dir}"
+    stow -R --target="$HOME" "${stow_dir}"
 done
