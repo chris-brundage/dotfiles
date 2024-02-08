6d8f2758e7bba128123d7587f1079cc600455e27
Chris Brundage <christopher.m.brundage@gmail.com>
2024-02-07 22:43:45 -0600

    Adds keyboard shortcut for toggling relative number in neovim

--- neovim/.config/nvim/lua/cbrundage/remap.lua
+++ neovim/.config/nvim/lua/cbrundage/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Toggle relativenumber on and off
+vim.keymap.set('n', '<C-n><C-n>', function ()
+    local option_value = vim.api.nvim_get_option_value('relativenumber', {})
+    vim.api.nvim_set_option_value('relativenumber', not option_value, {})
+end)
