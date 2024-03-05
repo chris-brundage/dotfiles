dd4b2d62a53619127c43f47b194a67a9348dc5eb
Chris Brundage <christopher.m.brundage@gmail.com>
2024-03-04 18:20:46 -0600

    Adds script for bootstrapping homebrew on new laptops

---
+++ homebrew-install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+if [[ ! "$(uname -s)" == "Darwin" ]]; then
+    printf "This script is only meant for macOS systems\n"
+    exit 1
+fi
+
+MACOS_ARCH=$(uname -m)
+
+# Figure out what login shell (and therefore profile file) the user is using
+if [[ "${SHELL}" == "/bin/zsh" ]]; then
+    PROFILE_FILE="${HOME}/.zprofile"
+elif [[ "${SHELL}" == *bash* ]]; then
+    PROFILE_FILE="${HOME}/.bash_profile"
+else
+    printf "Shell %s is not supported by this script!\n" "${SHELL}"
+    exit 1
+fi
+
+setup_homebrew() {
+    if ! command -v brew >/dev/null 2>&1; then
+        install_homebrew
+
+        if [[ "${MACOS_ARCH}" == "x86_64" ]]; then
+            brew_path="/usr/local/bin"
+        else
+            brew_path="/opt/homebrew/bin"
+        fi
+    else
+        brew_path="$(brew --prefix)/bin"
+    fi
+
+    if ! env | grep -q "${brew_path}"; then
+        # shellcheck disable=SC1090
+        source "${PROFILE_FILE}"
+    fi
+}
+
+install_homebrew() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+# Install macOS developer tools if necessary
+xcode_install() {
+    if ! command -v git >/dev/null 2>&1; then
+        printf "Developer tools are not installed. Installing them now!\n"
+        sleep 5
+
+        sudo xcode-select --install
+    fi
+}
+
+xcode_install
+setup_homebrew
+
+printf "Successfully installed homebrew\n"
