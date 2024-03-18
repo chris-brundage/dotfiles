023d3fa7e156977f6f5cf41d2a31293f63e068eb
Chris Brundage <christopher.m.brundage@gmail.com>
2024-03-18 17:38:54 -0500

    Make wezterm keybindings match iTerm expectations

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,6 +14,28 @@ config.keys = {
     mods = 'CMD|SHIFT',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
+  -- Make option + left arrow jump backwards a word
+  {
+    key = 'LeftArrow',
+    mods = 'META',
+    action=wezterm.action{SendString="\x1bb"},
+  },
+  -- Make option + right arrow jump forwards a word
+  {
+    key = 'RightArrow',
+    mods = 'META',
+    action=wezterm.action{SendString="\x1bf"},
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action=wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action=wezterm.action.ActivateTabRelative(1)
+  },
 }
 
 return config
