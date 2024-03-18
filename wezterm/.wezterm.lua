41f0390e56c84cee3b917bc3f2ee456a1f5c1ae7
Chris Brundage <christopher.m.brundage@gmail.com>
2024-03-17 21:22:51 -0500

    Let's try out wezterm why not

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'iTerm2 Pastel Dark Background'
+
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
+
+return config
