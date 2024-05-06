d401aaf11a167a4313e34395c9931f3bfd354797
Chris Brundage <christopher.m.brundage@gmail.com>
2024-05-05 21:34:18 -0500

    Emulates netrw's Vex and Ex with oil

--- neovim/.config/nvim/after/plugin/oil.lua
+++ neovim/.config/nvim/after/plugin/oil.lua
@@ -1,5 +1,18 @@
-require("oil").setup({
+local oil = require('oil')
+
+oil.setup({
   view_options = {
     show_hidden = true,
   }
 })
+
+-- Allow my muscle memory from netrw to still work
+vim.api.nvim_create_user_command('Ex', function ()
+  oil.open(vim.fn.expand('%:p:h'))
+end, {})
+
+vim.api.nvim_create_user_command('Vex', function ()
+  vim.cmd('vsp ' .. vim.fn.expand('%:p:h'))
+end, {})
+
+vim.keymap.set('n', '<leader>b', '<cmd>Oil --float<cr>', {})
