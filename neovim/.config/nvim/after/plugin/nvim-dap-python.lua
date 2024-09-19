b480d4597c33b28f1d7db5e847bc904183ca2c02
Chris Brundage <chris.brundage@atmosphere.tv>
2024-09-19 13:57:14 -0500

    More fun with dap

--- neovim/.config/nvim/after/plugin/nvim-dap-python.lua
+++ neovim/.config/nvim/after/plugin/nvim-dap-python.lua
@@ -1,1 +1,20 @@
-require("dap-python").setup("python")
+dap_python = require("dap-python")
+
+dap_python.setup("python")
+dap_python.resolve_python = function ()
+  if vim.fn.executable("pyenv") then
+    return io.popen("pyenv which python")
+  end
+
+  if os.getenv("VIRTUALENV") then
+    return os.getenv("VIRTUALENV")
+  end
+
+  if os.getenv("PYTHON_PATH") then
+    return os.getenv("PYTHON_PATH")
+  end
+
+  if vim.fn.isdirectory(".venv") then
+    return ".venv/bin/python"
+  end
+end
