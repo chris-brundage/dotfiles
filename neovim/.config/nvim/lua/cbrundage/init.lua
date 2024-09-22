5ec197ac180ca85a8355f307b1e1874f59df83be
Chris Brundage <christopher.m.brundage@gmail.com>
2024-09-22 11:29:55 -0500

    nvim: adds some Gentoo file type detection niceties

--- neovim/.config/nvim/lua/cbrundage/init.lua
+++ neovim/.config/nvim/lua/cbrundage/init.lua
@@ -11,4 +11,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+--
+-- Gentoo specific stuff
+--
+local gentoo_group = vim.api.nvim_create_augroup('Gentoo', { clear = true })
+
+local portage_conf_dir = '/etc/portage'
+for _, event in pairs({ 'BufRead', 'BufNewFile' }) do
+  -- Ensure config files in /etc/portage get treated as such
+  vim.api.nvim_create_autocmd(event, {
+    pattern = { portage_conf_dir .. '/package.*' },
+    callback = function()
+      vim.opt.filetype = 'conf'
+    end,
+    group = gentoo_group
+  })
+
+  -- Same for bashrc files in /etc/portage
+  vim.api.nvim_create_autocmd(event, {
+    pattern = { portage_conf_dir .. '*bashrc', portage_conf_dir .. '/env/*' },
+    callback = function()
+      vim.opt.filetype = 'bash'
+    end,
+    group = gentoo_group
+  })
+end
+--
+-- End Gentoo stuff
+--
+
 vim.cmd [[colorscheme catppuccin-mocha]]
