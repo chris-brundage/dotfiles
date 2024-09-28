2613720f32caf9fb3332ad8ef8ed7bdca7153cec
Chris Brundage <christopher.m.brundage@gmail.com>
2024-09-28 09:42:40 -0500

    What was I thinking with the /etc/portage filetype specification stuff?

---
+++ neovim/.config/nvim/after/ftplugin/portage.lua
@@ -0,0 +1,8 @@
+local portage_conf_dir = '/etc/portage'
+vim.filetype.add({
+  pattern = {
+    [portage_conf_dir .. '/package.*'] = 'conf',
+    [portage_conf_dir .. '/.*bashrc'] = 'bash',
+    [portage_conf_dir .. '/env/.*'] = 'bash'
+  }
+})
