798f4740db08973b457c9a6a345f4869f6cb1752
Chris Brundage <chris.brundage@atmosphere.tv>
2024-09-30 07:30:22 -0500

    Looks like I hit the lua autoformatted while adding a new keyboard shortcut to neovim

--- neovim/.config/nvim/lua/cbrundage/remap.lua
+++ neovim/.config/nvim/lua/cbrundage/remap.lua
@@ -5,9 +5,9 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Toggle relativenumber on and off
-vim.keymap.set('n', '<C-n><C-n>', function ()
-    local option_value = vim.api.nvim_get_option_value('relativenumber', {})
-    vim.api.nvim_set_option_value('relativenumber', not option_value, {})
+vim.keymap.set('n', '<C-n><C-n>', function()
+  local option_value = vim.api.nvim_get_option_value('relativenumber', {})
+  vim.api.nvim_set_option_value('relativenumber', not option_value, {})
 end)
 
 -- Search for files using fzf
@@ -19,13 +19,18 @@ vim.keymap.set('n', '<leader>SS', '<cmd>RG<cr>', {})
 vim.keymap.set('n', '<leader>gf', '<cmd>GFiles<cr>', { desc = 'Search [G]it [F]iles' })
 
 -- Thanks Primeagen
-vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]])
-vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
-vim.keymap.set({'n'}, '<leader>dp', function ()
+-- dap
+vim.keymap.set({ 'n' }, '<leader>dp', function()
   require('dap').continue()
 end)
+vim.keymap.set({ 'n' }, '<leader>dt', function() require('dapui').toggle() end)
 
-vim.keymap.set({'n'}, '<leader>dt', function() require('dapui').toggle() end)
+-- Cycle through buffers
+vim.keymap.set({ 'n' }, '<leader>Bn', '<cmd>bnext<cr>')
+vim.keymap.set({ 'n' }, '<leader>Bp', '<cmd>bprevious<cr>')
+vim.keymap.set({ 'n' }, '<leader>Bd', '<cmd>bdelete<cr>')
