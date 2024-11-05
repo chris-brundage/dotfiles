287475acbad7a25bd2402f3864e310aa29bf8487
Chris Brundage <chris.brundage@atmosphere.tv>
2024-11-05 10:51:07 -0600

    Adds Git push keyboard shortcut

--- neovim/.config/nvim/after/plugin/fugitive.lua
+++ neovim/.config/nvim/after/plugin/fugitive.lua
@@ -4,4 +4,4 @@ end
 
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 vim.keymap.set('n', '<leader>ga', vim.cmd.Gwrite)
--- vim.keymap.set('n', '<leader>gc', vim.cmd('Git commit'))
+vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
