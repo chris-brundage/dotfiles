0d701e52ecbb92f76d4b66a3a390e972841b0a9e
Chris Brundage <cbrundage@clarifying.com>
2025-10-23 17:25:31 -0400

    Switches color scheme back to catppuccin after like a year of rosepine

--- neovim/.config/nvim/lua/cbrundage/init.lua
+++ neovim/.config/nvim/lua/cbrundage/init.lua
@@ -12,4 +12,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- vim.cmd [[colorscheme tokyonight-night]]
-vim.cmd [[colorscheme rose-pine-moon]]
+-- vim.cmd [[colorscheme rose-pine-moon]]
+vim.cmd [[colorscheme catppuccin ]]
