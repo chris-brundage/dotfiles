3392f36aca97bd6bc29c2a4c0919277ea01c4115
Chris Brundage <chris.brundage@sinch.com>
2026-06-01 17:17:14 -0400

    Command alias for resizing floaterm to my usual values

--- neovim/.config/nvim/after/plugin/floaterm.lua
+++ neovim/.config/nvim/after/plugin/floaterm.lua
@@ -1,1 +1,12 @@
-vim.keymap.set({'n', 't'}, '<leader>tt', '<cmd>FloatermToggle<cr>')
+vim.keymap.set({ 'n', 't' }, '<leader>tt', '<cmd>FloatermToggle<cr>')
+
+
+-- Termsize sets 140x50
+-- Termsize 140 sets width to 140
+-- Termsize 140 50 sets 140x50
+vim.api.nvim_create_user_command("Termsize", function(opts)
+  local args = vim.split(opts.args, "%s+", { trimempty = true })
+  local width = args[1] or "140"
+  local height = args[2] or "50"
+  vim.cmd("FloatermUpdate --width=" .. width .. " --height=" .. height)
+end, { nargs = "*" })
