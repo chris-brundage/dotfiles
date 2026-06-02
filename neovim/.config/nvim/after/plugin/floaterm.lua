74e1b3328bfb1ed228cc1a074c8d9c6b2a1ba55b
Chris Brundage <chris.brundage@sinch.com>
2026-06-02 12:50:54 -0400

    Keyboard shortcut for Termsize

--- neovim/.config/nvim/after/plugin/floaterm.lua
+++ neovim/.config/nvim/after/plugin/floaterm.lua
@@ -4,9 +4,11 @@ vim.keymap.set({ 'n', 't' }, '<leader>tt', '<cmd>FloatermToggle<cr>')
 -- Termsize sets 140x50
 -- Termsize 140 sets width to 140
 -- Termsize 140 50 sets 140x50
-vim.api.nvim_create_user_command("Termsize", function(opts)
-  local args = vim.split(opts.args, "%s+", { trimempty = true })
-  local width = args[1] or "140"
-  local height = args[2] or "50"
-  vim.cmd("FloatermUpdate --width=" .. width .. " --height=" .. height)
-end, { nargs = "*" })
+vim.api.nvim_create_user_command('Termsize', function(opts)
+  local args = vim.split(opts.args, '%s+', { trimempty = true })
+  local width = args[1] or '140'
+  local height = args[2] or '50'
+  vim.cmd('FloatermUpdate --width=' .. width .. ' --height=' .. height)
+end, { nargs = '*' })
+
+vim.keymap.set('n', '<leader>ts', '<cmd>Termsize<cr>', { desc = 'Resize Floaterm' })
