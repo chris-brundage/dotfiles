9f937c04bf95c46753209be319caeeb493c54c3e
Chris Brundage <chris.brundage@sinch.com>
2026-08-04 13:17:18 -0400

    lol jk on being able to use tab properly with neogen

--- neovim/.config/nvim/after/plugin/neogen.lua
+++ neovim/.config/nvim/after/plugin/neogen.lua
@@ -17,28 +17,28 @@ vim.keymap.del('n', '<leader>d', { silent = true })
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<leader>dg", ":lua require('neogen').generate()<CR>", opts)
 
--- Tab between elements in the generated docstring
-cmp.setup {
-  mapping = {
-    ["<tab>"] = cmp.mapping(function(fallback)
-      if neogen.jumpable() then
-        neogen.jump_next()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-tab>"] = cmp.mapping(function(fallback)
-      if neogen.jumpable(true) then
-        neogen.jump_prev()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-  },
-}
+-- -- Tab between elements in the generated docstring
+-- cmp.setup {
+--   mapping = {
+--     ["<tab>"] = cmp.mapping(function(fallback)
+--       if neogen.jumpable() then
+--         neogen.jump_next()
+--       else
+--         fallback()
+--       end
+--     end, {
+--       "i",
+--       "s",
+--     }),
+--     ["<S-tab>"] = cmp.mapping(function(fallback)
+--       if neogen.jumpable(true) then
+--         neogen.jump_prev()
+--       else
+--         fallback()
+--       end
+--     end, {
+--       "i",
+--       "s",
+--     }),
+--   },
+-- }
