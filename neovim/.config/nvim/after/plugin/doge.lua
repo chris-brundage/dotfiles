b6185b5f2da5d062a4feae195496958a26aa0a68
Chris Brundage <chris.brundage@sinch.com>
2026-08-04 13:11:14 -0400

    Working docstring generation in neovim again

--- neovim/.config/nvim/after/plugin/doge.lua
+++
@@ -1,6 +0,0 @@
-if vim.g.vscode then
-    do return end
-end
-
-vim.keymap.del('n', '<leader>d', {silent = true})
-vim.api.nvim_set_keymap('n', '<leader>dg', '<Plug>(doge-generate)', {silent = true})
---
+++ neovim/.config/nvim/after/plugin/neogen.lua
@@ -0,0 +1,44 @@
+local neogen = require('neogen')
+local cmp = require('cmp')
+
+neogen.setup({
+  enabled = true,
+  languages = {
+    python = {
+      template = {
+        annotation_convention = 'google_docstrings'
+      }
+    }
+  }
+})
+
+vim.keymap.del('n', '<leader>d', { silent = true })
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<leader>dg", ":lua require('neogen').generate()<CR>", opts)
+
+-- Tab between elements in the generated docstring
+cmp.setup {
+  mapping = {
+    ["<tab>"] = cmp.mapping(function(fallback)
+      if neogen.jumpable() then
+        neogen.jump_next()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-tab>"] = cmp.mapping(function(fallback)
+      if neogen.jumpable(true) then
+        neogen.jump_prev()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+}
