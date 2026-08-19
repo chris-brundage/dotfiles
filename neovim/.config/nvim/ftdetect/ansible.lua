0715b15fd29b2fda0fc6307f9b47b8e4dca705c1
Chris Brundage <chris.brundage@sinch.com>
2026-08-19 11:49:58 -0400

    Ensures Github actions files are always set to regular yaml even in Ansible projects.

--- neovim/.config/nvim/ftdetect/ansible.lua
+++ neovim/.config/nvim/ftdetect/ansible.lua
@@ -2,6 +2,7 @@ local homedir = os.getenv('HOME')
 
 vim.filetype.add({
   pattern = {
+    ['.*/%.github/.*%.yml'] = { 'yaml', { priority = 10 } },
     [homedir .. '/src/mf%-ansible/.*.yml'] = 'yaml.ansible',
     [homedir .. '/src/sinch%-mg%-infrastructure/ansible/.*.yml'] = 'yaml.ansible'
   }
